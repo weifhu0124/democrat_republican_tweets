bd6a0c6c4e9d1cc0c03c0d1bf04f39436a7c8fe0
Weifeng Hu <weifenghu@Weifengs-MacBook-Pro.local>
2019-05-30 11:28:06 -0700

    change path

---
+++ task2/supervise.py
@@ -0,0 +1,31 @@
+#!/bin/python
+def feature_engineering(sentiment):
+	"""Perform feature engineering on the data
+
+	:return: features for logistic regression
+	"""
+	from sklearn import preprocessing
+	sentiment.le = preprocessing.LabelEncoder()
+	sentiment.le.fit(sentiment.train_labels)
+	sentiment.target_labels = sentiment.le.classes_
+	sentiment.trainy = sentiment.le.transform(sentiment.train_labels)
+	sentiment.devy = sentiment.le.transform(sentiment.dev_labels)
+	from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+	from nltk.corpus import stopwords
+	stopWords = set(stopwords.words('english'))
+	#stopWords.remove('not')
+	#stopWords.remove('but')
+	#stopWords.add('chicago')
+	#stopWords.add('michigan')
+	sentiment.count_vect = CountVectorizer(stop_words=list(stopWords), ngram_range=(1,3),min_df=2)
+	sentiment.tfidf_vect = TfidfVectorizer(stop_words=list(stopWords), ngram_range=(1,3), min_df=2, sublinear_tf=True)
+	sentiment.trainX = sentiment.tfidf_vect.fit_transform(sentiment.train_data)
+	sentiment.devX = sentiment.tfidf_vect.transform(sentiment.dev_data)
+
+def process_unlabeled(unlabeled, sentiment):
+	"""Process the unlabeled data to have the same feature engineering procedure
+
+	:return: processed unlabeled data
+	"""
+	unlabeled.X = sentiment.tfidf_vect.transform(unlabeled.data)
+	return unlabeled
---
+++ task2/visualization.py
@@ -0,0 +1,75 @@
+import numpy as np
+import pickle
+from lime.lime_text import LimeTextExplainer
+from sklearn.pipeline import make_pipeline
+from matplotlib import pyplot as plt
+
+def get_terms(idx):
+	with open('model/cls.pkl', 'rb') as fp:
+		cls = pickle.load(fp)
+	with open('model/sentiment_tfidf.pkl', 'rb') as fp:
+		sentiment = pickle.load(fp)
+	print(cls.classes_)
+
+	idx = np.argsort(np.asarray(cls.coef_[0]))
+	terms = sentiment.vocabulary_
+	terms = {v: k for k, v in terms.items()}
+	positive_words = []
+	negative_words = []
+	for i in idx[:15]:
+		negative_words.append(terms[i])
+	for i in idx[-15:]:
+		positive_words.append(terms[i])
+	positive_words.reverse()
+	return positive_words, negative_words
+
+def get_lime():
+	with open('model/cls.pkl', 'rb') as fp:
+		cls = pickle.load(fp)
+	with open('model/sentiment_tfidf.pkl', 'rb') as fp:
+		sentiment_tfidf = pickle.load(fp)
+	with open('model/sentiment_dev.pkl', 'rb') as fp:
+		test_data = pickle.load(fp)
+	c = make_pipeline(sentiment_tfidf, cls)
+	explainer = LimeTextExplainer(class_names=['republican', 'democrat'])
+	idx = 231
+	sample = 'Chairman @RepKevinBrady on @FoxNews and @FoxBusiness to discuss the benefits of #TaxReform. Some highlights ⬇️ https://t.co/urDcaXeWjF'
+	exp = explainer.explain_instance(sample, c.predict_proba)
+	print('Document id: %d' % idx)
+	print('Probability(democrat) =', c.predict_proba([sample])[0, 1])
+	exp.save_to_file('html/result.html')
+
+
+
+def get_extreme_example():
+	with open('model/cls.pkl', 'rb') as fp:
+		cls = pickle.load(fp)
+	with open('model/sentiment_tfidf.pkl', 'rb') as fp:
+		sentiment_tfidf = pickle.load(fp)
+	with open('model/sentiment_train.pkl', 'rb') as fp:
+		train_data = pickle.load(fp)
+	c = make_pipeline(sentiment_tfidf, cls)
+	curmax = -1
+	curmin = -1
+	max_str = ''
+	min_str = ''
+	for i in range(len(train_data)):
+		prob = c.predict_proba([train_data[i]])[0, 1]
+		if prob > curmax:
+			curmax = prob
+			max_str = train_data[i]
+		if prob < curmin or curmin == -1:
+			curmin = prob
+			min_str = train_data[i]
+	print('Most democrat:', max_str)
+	print('Most republican:', min_str)
+	return max_str, min_str
+
+
+
+if __name__ == '__main__':
+	#get_extreme_example()
+	get_lime()
+	#pos, neg = get_terms(15)
+	#print('democrat words', pos)
+	#print('republican words', neg)
